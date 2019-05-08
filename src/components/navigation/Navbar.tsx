7aa175b6a4a7588b4a76bda8a83ecb2588be5efb
Folusho Oladipo <foluski08@gmail.com>
2019-05-08 15:17:32 +0100

    Create the navbar element.

---
+++ src/components/navigation/Navbar.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+export default class Navbar extends React.Component {
+    render() {
+        return (
+            <div className="navbar-container">This is the navbar</div>
+        )
+    }
+}
